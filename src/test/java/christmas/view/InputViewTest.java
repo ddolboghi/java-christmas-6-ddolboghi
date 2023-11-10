937bb33b83169f5a346e4b5d1dfafb1ad229054b
ddolboghi <rhdfyd128@gmail.com>
2023-11-10 16:50:05 +0900

    remove: 콘솔 출력을 캡처하는 코드 삭제

--- src/test/java/christmas/view/InputViewTest.java
+++ src/test/java/christmas/view/InputViewTest.java
@@ -5,8 +5,6 @@ import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 
 import camp.nextstep.edu.missionutils.Console;
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
@@ -15,30 +13,19 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 class InputViewTest {
     private InputView inputView;
-    private ByteArrayOutputStream outputStreamCaptor;
-    private PrintStream standardOut;
 
     @BeforeEach
     public void setUp() {
         inputView = new InputView();
-
-        standardOut = System.out;
-        outputStreamCaptor = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outputStreamCaptor));
     }
 
     @AfterEach
     public void restoreSettings() {
         Console.close();
-        System.setOut(standardOut);
     }
 
     private void inputValue(String value) {
         System.setIn(new ByteArrayInputStream(value.getBytes()));
-    }
-
-    private String getOutput() {
-        return outputStreamCaptor.toString();
     }
 
     @Nested
