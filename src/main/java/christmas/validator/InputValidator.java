d624da810d66196b7e1b1f248885148ce48701f3
ddolboghi <rhdfyd128@gmail.com>
2023-11-10 22:30:04 +0900

    feat: 주문메뉴와 개수 입력 검증 기능 구현

---
+++ src/main/java/christmas/validator/InputValidator.java
@@ -0,0 +1,53 @@
+package christmas.validator;
+
+import java.util.regex.Pattern;
+
+public class InputValidator {
+    private static final String COMMON_ERROR_MESSAGE = "[ERROR] 공백이 아닌 문자를 입력해주세요.\n";
+    private static final String VISIT_DATE_ERROR_MESSAGE = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.\n";
+    private static final String ORDER_MENU_ERROR_MESSAGE = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.\n";
+    private static final int MONTH_START_DATE = 1;
+    private static final int MONTH_END_DATE = 31;
+
+    private static final Pattern NUMERIC_PATTERN = Pattern.compile("\\d+");
+    private static final Pattern MENU_PATTERN = Pattern.compile("([가-힣]+-\\d+,)*([가-힣]+-\\d+)$");
+
+    public static void validateVisitDate(String userInputVisitDate) {
+        validateBlank(userInputVisitDate);
+        validateNumeric(userInputVisitDate);
+        validateDateRange(Integer.parseInt(userInputVisitDate));
+    }
+
+    public static void validateOrderMenu(String userInputOrderMenu) {
+        validateBlank(userInputOrderMenu);
+        validateMenuFormat(userInputOrderMenu);
+    }
+
+    private static void validateBlank(String input) {
+        if (input == null || input.isBlank()) {
+            throw new IllegalArgumentException(COMMON_ERROR_MESSAGE);
+        }
+    }
+
+    private static void validateNumeric(String userInputVisitDate) {
+        if (!NUMERIC_PATTERN.matcher(userInputVisitDate).matches()) {
+            throw new IllegalArgumentException(VISIT_DATE_ERROR_MESSAGE);
+        }
+    }
+
+    private static void validateDateRange(int parsedVisitDate) {
+        if (!isInRange(parsedVisitDate)) {
+            throw new IllegalArgumentException(VISIT_DATE_ERROR_MESSAGE);
+        }
+    }
+
+    private static boolean isInRange(int parsedVisitDate) {
+        return (MONTH_START_DATE <= parsedVisitDate) && (MONTH_END_DATE >= parsedVisitDate);
+    }
+
+    private static void validateMenuFormat(String userInputOrderMenu) {
+        if (!MENU_PATTERN.matcher(userInputOrderMenu).matches()) {
+            throw new IllegalArgumentException(ORDER_MENU_ERROR_MESSAGE);
+        }
+    }
+}
--- src/main/java/christmas/validator/OrderMenuValidator.java
+++
@@ -1,5 +0,0 @@
-package christmas.validator;
-
-public class OrderMenuValidator {
-    private static final String ORDER_MENU_ERROR_MESSAGE = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.\n";
-}
