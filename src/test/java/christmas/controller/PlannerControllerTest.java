ad7783e7fc4a873b17c73a544d0cb052bca8b99f
ddolboghi <rhdfyd128@gmail.com>
2023-11-10 22:42:57 +0900

    test: 입력 오류시 주문메뉴와 개수 재입력 기능 테스트

--- src/test/java/christmas/controller/PlannerControllerTest.java
+++ src/test/java/christmas/controller/PlannerControllerTest.java
@@ -29,8 +29,16 @@ class PlannerControllerTest {
     }
 
     @Test
-    void 정상적인_방문_날짜를_입력받을_때까지_재입력받는다() {
-        String userInputVisitDate = " \n1.23\n0\n32\n25";
+    void 잘못된_방문_날짜를_입력하면_정상적인_값을_입력받을_때까지_재입력받는다() {
+        String userInputVisitDate = " \n1.23\n0\n32\n25\n티본스테이크-1";
+        inputValue(userInputVisitDate);
+
+        assertDoesNotThrow(plannerController::preview);
+    }
+
+    @Test
+    void 잘못된_주문메뉴와_개수를_입력하면_정상적인_값을_입력받을_때까지_재입력받는다() {
+        String userInputVisitDate = " 25\n티본스테이크\n티본스테이크-0\n티본스테이크1\n티본스테이크 1\n티본스테이크:1\n티본스테이크-1";
         inputValue(userInputVisitDate);
 
         assertDoesNotThrow(plannerController::preview);
