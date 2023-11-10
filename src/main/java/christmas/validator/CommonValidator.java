0bf78f863ac6b2a9c88b9dcdbf0cf62e257579fd
ddolboghi <rhdfyd128@gmail.com>
2023-11-10 20:09:28 +0900

    fix: 오류 메시지 수정

--- src/main/java/christmas/validator/CommonValidator.java
+++ src/main/java/christmas/validator/CommonValidator.java
@@ -1,7 +1,7 @@
 package christmas.validator;
 
 public class CommonValidator {
-    private static final String COMMON_ERROR_MESSAGE = "[ERROR] 공백이 아닌 문자를 입력해주세요.";
+    private static final String COMMON_ERROR_MESSAGE = "[ERROR] 공백이 아닌 문자를 입력해주세요.\n";
 
     public static void validateBlank(String input) {
         if (input == null || input.isBlank()) {
--- src/main/java/christmas/validator/VisitDateValidator.java
+++ src/main/java/christmas/validator/VisitDateValidator.java
@@ -3,7 +3,7 @@ package christmas.validator;
 import java.util.regex.Pattern;
 
 public class VisitDateValidator extends CommonValidator {
-    private static final String VISIT_DATE_ERROR_MESSAGE = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+    private static final String VISIT_DATE_ERROR_MESSAGE = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.\n";
     private static final int MONTH_START_DATE = 1;
     private static final int MONTH_END_DATE = 31;
 
