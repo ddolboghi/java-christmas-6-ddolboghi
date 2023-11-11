604467fc7cc0e5136c87e80d18669ea33fc2b738
ddolboghi <rhdfyd128@gmail.com>
2023-11-12 03:03:34 +0900

    fix: OutputView의 플래너 제목 출력값 수정

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -4,7 +4,7 @@ import christmas.io.Output;
 
 public class OutputView {
     private static final String INTRODUCTION_OF_PLANNER = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
-    private static final String TITLE_OF_PLANNER = "12월 3일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
+    private static final String TITLE_OF_PLANNER = "12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
     private static final String TITLE_OF_ORDER_MENU = "<주문 메뉴>";
     private static final String TITLE_OF_TOTAL_COST_BEFORE_DISCOUNT = "<할인 전 총주문 금액>";
     private static final String TITLE_OF_PRESENT_MENU = "<증정 메뉴>";
