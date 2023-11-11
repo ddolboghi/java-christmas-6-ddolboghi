175d1364eb5ec113dc450328950ff98dd023a01c
ddolboghi <rhdfyd128@gmail.com>
2023-11-11 20:19:25 +0900

    feat: 식당 메뉴를 Enum으로 추가

---
+++ src/main/java/christmas/util/Menu.java
@@ -0,0 +1,38 @@
+package christmas.util;
+
+public enum Menu {
+    PINE_MUSHROOM_SOUP("양송이수프", "appetizer", 6000),
+    TAPAS("타파스", "appetizer", 5500),
+    CAESAR_SALAD("시저샐러드", "appetizer", 8000),
+    T_BONE_STEAK("티본스테이크", "main", 55000),
+    BARBECUE_RIBS("바비큐립", "main", 54000),
+    SEAFOOD_PASTA("해산물파스타", "main", 35000),
+    CHRISTMAS_PASTA("크리스마스파스타", "main", 25000),
+    CHOCOLATE_CAKE("초코케이크", "dessert", 15000),
+    ICE_CREAM("아이스크림", "dessert", 5000),
+    ZERO_COLA("제로콜라", "drink", 3000),
+    RED_WINE("레드와인", "drink", 60000),
+    CHAMPAGNE("샴페인", "drink", 25000);
+
+    private final String koreanName;
+    private final String category;
+    private final int price;
+
+    Menu(String koreanName, String category, int price) {
+        this.koreanName = koreanName;
+        this.category = category;
+        this.price = price;
+    }
+
+    public String getKoreanName() {
+        return koreanName;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
