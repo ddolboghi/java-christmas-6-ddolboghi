b4c89401f3263299e964ef2cce03d4e0839c92e7
ddolboghi <rhdfyd128@gmail.com>
2023-11-12 21:07:04 +0900

    refactor: OrderManager에 사용되는 상수들 분리

---
+++ src/main/java/christmas/util/OrderManagerConst.java
@@ -0,0 +1,10 @@
+package christmas.util;
+
+public class OrderManagerConst {
+    public static final String ORDER_DELIMITER = ",";
+    public static final String MENU_AND_AMOUNT_DELIMITER = "-";
+    public static final int INDEX_OF_MENU = 0;
+    public static final int INDEX_OF_AMOUNT = 1;
+    public static final String DESSERT_CATEGORY = "dessert";
+    public static final String MAIN_CATEGORY = "main";
+}
