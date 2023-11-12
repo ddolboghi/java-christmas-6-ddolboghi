fc8b7e8d14a52fd8fea782b3df0d1db20d5410ab
ddolboghi <rhdfyd128@gmail.com>
2023-11-12 21:08:00 +0900

    refactor: view에 사용되는 상수들 분리

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,12 +1,13 @@
 package christmas.view;
 
+import static christmas.view.OutputMessage.ASK_VISIT_DATE;
+import static christmas.view.OutputMessage.TAKE_ORDER;
+
 import christmas.io.Input;
 import christmas.io.Output;
 import christmas.validator.InputValidator;
 
 public class InputView {
-    private static final String ASK_VISIT_DATE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
-    private static final String TAKE_ORDER = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
 
     public String inputVisitDate() {
         Output.writeLine(ASK_VISIT_DATE);
---
+++ src/main/java/christmas/view/OutputMessage.java
@@ -0,0 +1,17 @@
+package christmas.view;
+
+public class OutputMessage {
+    public static final String ASK_VISIT_DATE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+    public static final String TAKE_ORDER = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
+    public static final String INTRODUCTION_OF_PLANNER = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+    public static final String TITLE_OF_PLANNER = "12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
+    public static final String TITLE_OF_ORDER_MENU = "\n<주문 메뉴>";
+    public static final String TITLE_OF_TOTAL_COST_BEFORE_DISCOUNT = "\n<할인 전 총주문 금액>";
+    public static final String TITLE_OF_PRESENT_MENU = "\n<증정 메뉴>";
+    public static final String TITLE_OF_BENEFITS = "\n<혜택 내역>";
+    public static final String TITLE_OF_TOTAL_BENEFITS_SUM = "\n<총혜택 금액>";
+    public static final String TITLE_OF_TOTAL_COST_AFTER_DISCOUNT = "\n<할인 후 예상 결제 금액>";
+    public static final String TITLE_OF_EVENT_BADGE = "\n<12월 이벤트 배지>";
+    public static final String MENU_AND_AMOUNT = "%s %d개\n";
+    public static final String MONETARY_UNIT = "%s원";
+}
