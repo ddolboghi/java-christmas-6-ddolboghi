d1bce5003d3c646f46b25654bfa8ed71414e8c7a
ddolboghi <rhdfyd128@gmail.com>
2023-11-13 03:32:25 +0900

    rename: events에서 rule로 패키지명 수정

--- src/main/java/christmas/util/rule/PresentationEventRule.java
+++ src/main/java/christmas/util/rule/PresentationEventRule.java
@@ -1,9 +1,8 @@
-package christmas.util.events;
+package christmas.util.rule;
 
 public class PresentationEventRule {
     public static final int TOTAL_COST_CRITERIA = 120000;
     public static final String PRESENTATION_EVENT_NAME = "증정 이벤트";
-    public static final String NON_PRESENTATION_EVENT_MENU = "없음";
-    public static final int DISCOUNT_OF_NOT_APPLIED_PRESENTATION_EVENT = 0;
+    public static final String NON_PRESENT_MENU = "없음";
     public static final String PRESENTATION_EVENT_MENU_AMOUNT = "%s 1개";
 }
