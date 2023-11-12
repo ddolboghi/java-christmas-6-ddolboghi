a515e90a26046ce473bfe0cf1016f191dacaf853
ddolboghi <rhdfyd128@gmail.com>
2023-11-12 12:42:59 +0900

    test: 방문날짜 및 주문메뉴 출력 테스트

--- src/test/java/christmas/controller/PlannerControllerTest.java
+++ src/test/java/christmas/controller/PlannerControllerTest.java
@@ -1,31 +1,44 @@
 package christmas.controller;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import camp.nextstep.edu.missionutils.Console;
 import christmas.view.InputView;
 import christmas.view.OutputView;
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class PlannerControllerTest {
-
     private PlannerController plannerController;
+    private ByteArrayOutputStream outputStreamCaptor;
+    private PrintStream standardOut;
 
     @BeforeEach
     public void setPlannerController() {
         plannerController = new PlannerController(new InputView(), new OutputView());
+
+        standardOut = System.out;
+        outputStreamCaptor = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStreamCaptor));
     }
 
     @AfterEach
     public void restoreSettings() {
         Console.close();
+        System.setOut(standardOut);
     }
 
     private void inputValue(String value) {
         System.setIn(new ByteArrayInputStream(value.getBytes()));
+    }
+
+    private String getOutput() {
+        return outputStreamCaptor.toString();
     }
 
     @Test
@@ -42,5 +55,15 @@ class PlannerControllerTest {
         inputValue(userInputVisitDate);
 
         assertDoesNotThrow(plannerController::preview);
+    }
+
+    @Test
+    void 정상적인_방문날짜와_주문을_입력하면_방문날짜와_주문메뉴를_출력한다() {
+        String userInputVisitDate = " 25\n티본스테이크-1,해산물파스타-1,레드와인-1,초코케이크-1";
+        inputValue(userInputVisitDate);
+
+        plannerController.preview();
+
+        assertThat(getOutput()).contains("25", "티본스테이크 1개", "해산물파스타 1개", "레드와인 1개", "초코케이크 1개");
     }
 }
