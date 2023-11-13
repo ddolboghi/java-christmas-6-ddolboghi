1af6c949f23ba92f1b3e86f44ae978827087b2e1
ddolboghi <rhdfyd128@gmail.com>
2023-11-13 14:11:11 +0900

    refactor: Presentation을 Gift로 수정

--- src/main/java/christmas/util/rule/PresentationEventRule.java
+++
@@ -1,8 +0,0 @@
-package christmas.util.rule;
-
-public class PresentationEventRule {
-    public static final int TOTAL_COST_CRITERIA = 120000;
-    public static final String PRESENTATION_EVENT_NAME = "증정 이벤트";
-    public static final String NON_PRESENT_MENU = "없음";
-    public static final String PRESENTATION_EVENT_MENU_AMOUNT = "%s 1개";
-}
