03940d6e9fa6acb04bede0276e4484fd74de95cd
ddolboghi <rhdfyd128@gmail.com>
2023-11-13 21:21:42 +0900

    test: 총혜택 금액 계산 기능 테스트 코드 작성

--- src/test/java/christmas/model/BenefitTest.java
+++ src/test/java/christmas/model/BenefitTest.java
@@ -11,6 +11,8 @@ import christmas.model.event.WeekendDiscountEvent;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class BenefitTest {
 
@@ -18,24 +20,23 @@ class BenefitTest {
         return new Benefit(events);
     }
 
+    private List<Event> createEvents(int totalCost, int visitDate, int weekdayMenuAmount, int weekendMenuAmount) {
+        return List.of(
+                new GiftEvent(totalCost),
+                new SpecialDiscountEvent(visitDate),
+                new WeekdayDiscountEvent(visitDate, weekdayMenuAmount),
+                new WeekendDiscountEvent(visitDate, weekendMenuAmount),
+                new ChristmasDDayDiscountEvent(visitDate)
+        );
+    }
+
     @Test
     void 이벤트들중_적용되는_이벤트만_가져온다() {
-        Event giftEvent = new GiftEvent(10000);//이벤트 적용 안됨
-        Event specialDiscountEvent = new SpecialDiscountEvent(25);
-        Event weekdayDisCountEvent = new WeekdayDiscountEvent(25, 1);
-        Event weekendDiscountEvent = new WeekendDiscountEvent(25, 1);//이벤트 적용 안됨
-        Event christmasDDayDiscountEvent = new ChristmasDDayDiscountEvent(25);
-        List<Event> events = List.of(
-                giftEvent,
-                specialDiscountEvent,
-                weekdayDisCountEvent,
-                weekendDiscountEvent,
-                christmasDDayDiscountEvent
-        );
+        List<Event> events = createEvents(10000, 25, 1, 1);
         List<Event> appliedEvents = List.of(
-                specialDiscountEvent,
-                weekdayDisCountEvent,
-                christmasDDayDiscountEvent
+                new SpecialDiscountEvent(25),
+                new WeekdayDiscountEvent(25, 1),
+                new ChristmasDDayDiscountEvent(25)
         );
         Benefit benefit = createBenefit(events);
 
@@ -44,23 +45,21 @@ class BenefitTest {
 
     @Test
     void 적용되는_이벤트가_하나도_없으면_빈_배열을_반환한다() {
-        Event giftEvent = new GiftEvent(10000);
-        Event specialDiscountEvent = new SpecialDiscountEvent(1);
-        Event weekdayDisCountEvent = new WeekdayDiscountEvent(1, 1);
-        Event weekendDiscountEvent = new WeekendDiscountEvent(25, 1);
-        Event christmasDDayDiscountEvent = new ChristmasDDayDiscountEvent(26);
-        List<Event> events = List.of(
-                giftEvent,
-                specialDiscountEvent,
-                weekdayDisCountEvent,
-                weekendDiscountEvent,
-                christmasDDayDiscountEvent
-        );
+        List<Event> events = createEvents(55000, 28, 0, 1);
         Benefit benefit = createBenefit(events);
         List<Event> emptyAppliedEvents = new ArrayList<>();
 
         List<Event> appliedEvents = benefit.getAppliedEvents();
 
         assertThat(appliedEvents).isEqualTo(emptyAppliedEvents);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"8000, 25, 0, 0, 4400", "55000, 28, 0, 1, 0"})
+    void 총혜택_금액을_계산한다(int totalCost, int visitDate, int weekdayMenuAmount, int weekendMenuAmount, int totalDiscounts) {
+        List<Event> events = createEvents(totalCost, visitDate, weekdayMenuAmount, weekendMenuAmount);
+        Benefit benefit = createBenefit(events);
+
+        assertThat(benefit.sumDiscounts()).isEqualTo(totalDiscounts);
     }
 }
