0eb571a7e8bf877b7152f115dd2a2fca4edcc7bf
ddolboghi <rhdfyd128@gmail.com>
2023-11-14 00:30:46 +0900

    test: 배지 부여 기능 테스트 코드 작성

--- src/test/java/christmas/model/EventManagerTest.java
+++ src/test/java/christmas/model/EventManagerTest.java
@@ -44,11 +44,8 @@ class EventManagerTest {
         appliedEvents.put("평일 할인", 2023);
         appliedEvents.put("특별 할인", 1000);
 
-        //when
-        Map<String, Integer> benefits = eventManager.getBenefits();
-
-        //then
-        assertThat(benefits).isEqualTo(appliedEvents);
+        //when then
+        assertThat(eventManager.getBenefits()).isEqualTo(appliedEvents);
     }
 
     @Test
@@ -59,25 +56,38 @@ class EventManagerTest {
         when(orderManager.getMenuAmount("main")).thenReturn(0);
         eventManager = new EventManager(orderManager, 30);
 
-        //when
-        Map<String, Integer> benefits = eventManager.getBenefits();
-
-        //then
-        assertTrue(benefits.isEmpty());
+        //when then
+        assertTrue(eventManager.getBenefits().isEmpty());
     }
 
     @Test
-    void 할인_후_예상_결제_금액을_계산한다() {
-        //given
+    void 총혜택_금액을_계산한다() {
         when(orderManager.getTotalCost()).thenReturn(158000);
         when(orderManager.getMenuAmount("dessert")).thenReturn(0);
         when(orderManager.getMenuAmount("main")).thenReturn(2);
         eventManager = new EventManager(orderManager, 25);
 
-        //when
-        int totalCostAfterDiscount = eventManager.calculateTotalCostAfterDiscount();
+        assertThat(eventManager.getTotalDiscount()).isEqualTo(29400);
+    }
 
-        //then
-        assertThat(totalCostAfterDiscount).isEqualTo(153600);
+    @Test
+    void 할인_후_예상_결제_금액을_계산한다() {
+        when(orderManager.getTotalCost()).thenReturn(158000);
+        when(orderManager.getMenuAmount("dessert")).thenReturn(0);
+        when(orderManager.getMenuAmount("main")).thenReturn(2);
+        eventManager = new EventManager(orderManager, 25);
+
+        assertThat(eventManager.calculateTotalCostAfterDiscount()).isEqualTo(153600);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"158000, 0, 2, 25, 산타", "50000, 5, 1, 31, 트리", "45000, 3, 0, 26, 별", "55000, 0, 1, 28, 없음"})
+    void 총혜택_금액에따라_배지를_부여한다(int totalCost, int dessertAmount, int mainAmount, int visitDate, String badge) {
+        when(orderManager.getTotalCost()).thenReturn(totalCost);
+        when(orderManager.getMenuAmount("dessert")).thenReturn(dessertAmount);
+        when(orderManager.getMenuAmount("main")).thenReturn(mainAmount);
+        eventManager = new EventManager(orderManager, visitDate);
+
+        assertThat(eventManager.grantBadge()).isEqualTo(badge);
     }
 }
