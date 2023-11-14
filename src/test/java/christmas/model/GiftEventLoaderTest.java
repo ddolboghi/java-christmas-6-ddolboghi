dc109b32c3c283cb10eab2a277fa4d76a421d258
ddolboghi <rhdfyd128@gmail.com>
2023-11-15 02:08:27 +0900

    test: 증정메뉴 반환 기능 테스트 코드 작성

---
+++ src/test/java/christmas/model/GiftEventLoaderTest.java
@@ -0,0 +1,15 @@
+package christmas.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class GiftEventLoaderTest {
+
+    @ParameterizedTest
+    @CsvSource({"120000, 샴페인 1개", "119999, 없음"})
+    void 증정이벤트_적용여부에따라_증정메뉴를_반환한다(int totalCost, String giftMenu) {
+        assertThat(GiftEventLoader.getGiftMenu(totalCost)).isEqualTo(giftMenu);
+    }
+}
