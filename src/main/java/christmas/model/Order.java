17bb809173d0b001da77295b49e9615df7c2d1ce
ddolboghi <rhdfyd128@gmail.com>
2023-11-15 15:13:31 +0900

    refactor: Order의 할인 후 예상 결제 금액 계산 메서드명 수정

--- src/main/java/christmas/model/Order.java
+++ src/main/java/christmas/model/Order.java
@@ -25,7 +25,7 @@ public class Order {
         takeOrder();
     }
 
-    public int calculateCostAfterDiscount(int totalDiscountExceptGiftEvent) {
+    public int getTotalCostAfterDiscount(int totalDiscountExceptGiftEvent) {
         return getTotalCost() - totalDiscountExceptGiftEvent;
     }
 
